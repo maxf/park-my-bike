44eb2fe06d0ed421d9f13757f4bbbe195c5b37e5
Max <max@froumentin.net>
2014-10-25 16:54:23 +0100

    use promises

--- api/controllers/TwitterController.js
+++ api/controllers/TwitterController.js
@@ -9,45 +9,14 @@ var request = require('request');
 
 module.exports = {
 
-  get_tweets: function () {
-    return 'yay';
-  },
-
   twitter: function (req, res) {
     'use strict';
-    var t = sails.config.twitter;
-
-    // URL encode key/secret
-    var enc_key	= encodeURIComponent(t.api_key);
-    var enc_secret = encodeURIComponent(t.api_secret);
-
-    // Concatenate key/secret (seperated with colon)
-    var keysecret = enc_key + ':' + enc_secret;
-
-    // Base64 encode key/secret string
-    var keysecret_64 = new Buffer(keysecret).toString('base64');
-
-    // oAuth URL
-    var url = "https://api.twitter.com/oauth2/token";
-
-    // oAuth POST header
-    var headers = {
-      'Content-Type':'application/x-www-form-urlencoded;charset=UTF-8',
-      'Authorization':'Basic '+ keysecret_64
-    };
-
-    request.post({url:url,headers:headers,formData: {'grant_type':'client_credentials'}}, function(error, response, body) {
-      if (!error) {
-        var token = JSON.parse(body).access_token;
-      // get tweets
-        request({'url':'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=therealmaxf',
-                 'headers': { 'Authorization':'Bearer ' + token }
-                }, function (error, response, body) {
-                  if (!error) {
-                    return res.send(body);
-                  }
-                });
-     }
-    });
+    sails.services.twitter.getToken()
+      .then(function(authObject) {
+        sails.services.twitter.getTweets(authObject, req.query.lat, req.query.lon)
+        .then(function(tweets) {
+          return res.send(tweets);
+        });
+      });
   }
 };
